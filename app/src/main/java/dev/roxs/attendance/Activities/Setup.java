d5d11e503aac82b49ba6c7804267c4acdf252839
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-03 12:36:39 +0530

    Base setup

--- app/src/main/java/dev/roxs/attendance/Activities/Setup.java
+++ app/src/main/java/dev/roxs/attendance/Activities/Setup.java
@@ -1,14 +1,16 @@
-package dev.roxs.attendance;
+package dev.roxs.attendance.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import dev.roxs.attendance.R;
+
+public class Setup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setup);
     }
 }
