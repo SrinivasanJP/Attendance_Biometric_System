4b856c2266e325abb77631ed36dd26c97ae0c4dc
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-04 13:05:36 +0530

    design bug fix java and xml commit changes

--- app/src/main/java/dev/roxs/attendance/Activities/Setup.java
+++ app/src/main/java/dev/roxs/attendance/Activities/Setup.java
@@ -3,14 +3,36 @@ package dev.roxs.attendance.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import dev.roxs.attendance.R;
 
 public class Setup extends AppCompatActivity {
+    private EditText vReg_no, vName;
+    private ProgressBar vSetupProgress;
+    private TextView vFinishSetup;
+    private RelativeLayout finishSetupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        vReg_no = findViewById(R.id.reg_no);
+        vName = findViewById(R.id.name);
+        vSetupProgress = findViewById(R.id.setupprogressBar);
+        vSetupProgress.setVisibility(View.INVISIBLE);
+
+        finishSetupBtn = findViewById(R.id.finishSetupBtn);
+        finishSetupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
