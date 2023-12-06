d42109bdbe11378ec61f7e02f09c7688dab02e5e
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-06 11:28:40 +0530

    all the device Hardware data collected to encryption

--- app/src/main/java/dev/roxs/attendance/Helper/FingerPrint.java
+++ app/src/main/java/dev/roxs/attendance/Helper/FingerPrint.java
@@ -1,21 +1,26 @@
 package dev.roxs.attendance.Helper;
 
+import android.content.Context;
 import android.os.Build;
-
+import android.util.DisplayMetrics;
 
 public class FingerPrint {
-
-    public FingerPrint() {
+    private final Context mContext;
+    public FingerPrint(Context context) {
+        mContext = context;
     }
     public void getFingerPrint(){
 
+        DisplayMetrics displayMetrics = mContext.getResources().getDisplayMetrics();
+        int displayWidth = displayMetrics.widthPixels;
+        int displayHeight = displayMetrics.heightPixels;
         String deviceHardwareConfig = Build.BOARD + "_" +
                 Build.BRAND + "_" +
                 Build.DEVICE + "_" +
                 Build.HARDWARE + "_" +
                 Build.MANUFACTURER + "_" +
                 Build.MODEL + "_" +
-                Build.PRODUCT + "_" ;
+                Build.PRODUCT + "_" +displayWidth+"x"+displayHeight;
 
     }
 
