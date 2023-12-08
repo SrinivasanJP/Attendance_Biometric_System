38aeb09ffc0f68507ab3f82c0dd9fc78ddc5688b
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-08 11:00:39 +0530

    database data collected and tested

--- app/src/main/java/dev/roxs/attendance/Activities/Setup.java
+++ app/src/main/java/dev/roxs/attendance/Activities/Setup.java
@@ -1,5 +1,6 @@
 package dev.roxs.attendance.Activities;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,8 +10,13 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import dev.roxs.attendance.Helper.FingerPrint;
 import dev.roxs.attendance.R;
@@ -20,22 +26,46 @@ public class Setup extends AppCompatActivity {
     private EditText vReg_no, vName;
     private ProgressBar vSetupProgress;
     private TextView vFinishSetup;
-    private RelativeLayout finishSetupBtn;
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private DocumentReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+        //hooks
         vReg_no = findViewById(R.id.reg_no);
         vName = findViewById(R.id.name);
         vSetupProgress = findViewById(R.id.setupprogressBar);
-        vSetupProgress.setVisibility(View.INVISIBLE);
+        vFinishSetup = findViewById(R.id.finishSetupText);
+        RelativeLayout finishSetupBtn = findViewById(R.id.finishSetupBtn);
+        
+       
+        
 
-        finishSetupBtn = findViewById(R.id.finishSetupBtn);
+        //Finish setup button click
         finishSetupBtn.setOnClickListener(view -> {
+            vFinishSetup.setVisibility(View.INVISIBLE);
+            vSetupProgress.setVisibility(View.VISIBLE);
+            String sReg_no = vReg_no.getText().toString();
+            String sName = vName.getText().toString();
             FingerPrint fp = new FingerPrint(Setup.this);
+            
+            //document reference
+            reference = db.collection("users").document(sReg_no);
+
+            Map<String, String> user = new HashMap<>();
+            user.put("name", sName);
+            user.put("fingerprint",fp.getFingerPrint());
+            reference.set(user).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    Toast.makeText(Setup.this, "Data collected", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(Setup.this, "Problem occurs", Toast.LENGTH_SHORT).show();
+                }
+            });
+           
             Log.d("UT", fp.getFingerPrint());
         });
 
