1358ec628546e619fe36374fcf2884075e36a645
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-10 12:11:54 +0530

    QR code base

--- app/src/main/java/dev/roxs/attendance/Activities/IDPage.java
+++ app/src/main/java/dev/roxs/attendance/Activities/IDPage.java
@@ -1,16 +1,91 @@
 package dev.roxs.attendance.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import dev.roxs.attendance.R;
 
 public class IDPage extends AppCompatActivity {
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 200;
 
+
+    private RelativeLayout markAttendanceBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idpage);
+        //hooks
+        markAttendanceBtn = findViewById(R.id.markAttendanceBtn);
+        markAttendanceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(IDPage.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Request camera permission
+                    ActivityCompat.requestPermissions(IDPage.this,
+                            new String[]{Manifest.permission.CAMERA},
+                            CAMERA_PERMISSION_REQUEST_CODE);
+                } else {
+                    // Permission already granted, start scanning
+//                    startScanner();
+                    startActivity(new Intent(getApplicationContext(), QRReader.class));
+                }
+            }
+        });
+
+
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, start scanning
+                startScanner();
+            } else {
+                // Permission denied, show a message or handle accordingly
+                Toast.makeText(this, "Camera permission is required to scan QR codes",
+                        Toast.LENGTH_SHORT).show();
+                // You might want to close the activity or provide an alternative
+            }
+        }
+    }
+    private void startScanner() {
+
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setPrompt("Scan a QR code ");
+        integrator.setBeepEnabled(true);
+        integrator.setCameraId(0); // Use the default camera
+        integrator.setOrientationLocked(false); // Allow orientation rotation
+        integrator.initiateScan();
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                // Handle canceled scan
+                Toast.makeText(this, "Scan canceled", Toast.LENGTH_SHORT).show();
+            } else {
+                // Handle scan result (result.getContents())
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
