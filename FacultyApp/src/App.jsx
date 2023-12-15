24a81dea806d99ccd0bb3865c732195597cda010
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-15 10:59:53 +0530

    Tailwind bug fix

--- FacultyApp/src/App.jsx
+++ FacultyApp/src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { useState } from 'react'
 
-const App = () => {
+function App() {
+
   return (
-    <div>App</div>
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
   )
 }
 
