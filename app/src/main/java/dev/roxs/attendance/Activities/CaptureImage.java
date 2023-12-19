5785bab8650d3fd60ebe36b4732e163e91e63023
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-19 11:37:06 +0530

    camera comment removed

--- app/src/main/java/dev/roxs/attendance/Activities/CaptureImage.java
+++ app/src/main/java/dev/roxs/attendance/Activities/CaptureImage.java
@@ -41,30 +41,6 @@ public class CaptureImage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture_image);
         preIntent = getIntent();
-
-//        camera_open_id = findViewById(R.id.camera_button);
-//        click_image_id = findViewById(R.id.click_image);
-//
-//        // Camera_open button is for open the camera and add the setOnClickListener in this button
-//        camera_open_id.setOnClickListener(v -> {
-//            // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
-//            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//            // Start the activity with camera_intent, and request pic id
-//            startActivityForResult(camera_intent, pic_id);
-//        });
-//    }
-//
-//    // This method will help to retrieve the image
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        // Match the request 'pic id with requestCode
-//        if (requestCode == pic_id) {
-//            // BitMap is data structure of image file which store the image in memory
-//            Bitmap photo = (Bitmap) data.getExtras().get("data");
-//            // Set the image in imageview for display
-//            click_image_id.setImageBitmap(photo);
-//        }
-//    }
         previewView = findViewById(R.id.previewView);
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
