bb5a4082f2553eb67b956866c6297cea89dde6cd
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-22 12:26:56 +0530

    home page navigation

--- FacultyApp/src/App.jsx
+++ FacultyApp/src/App.jsx
@@ -33,7 +33,10 @@ function App() {
     <div className='flex justify-between m-4 rounded-2xl bg-gray-200 py-5 px-10 items-center'>
     
       <div>
-      <h1 className=' font-semibold  text-2xl'>VAttendance</h1>
+      <h1 className=' font-semibold  text-2xl cursor-pointer'  onClick={()=>{
+        setFragment("default");
+        setSessionID("NA")
+        }}>VAttendance</h1>
       <h2 className=' font-semibold'>Session ID : <span className=' font-mono'>{sessionID}</span></h2>
       </div>
       
