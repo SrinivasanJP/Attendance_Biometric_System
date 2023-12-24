e59d903f2838c03da89ec33cfb1124d089d17ff1
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-24 11:33:43 +0530

    localstorage check

--- FacultyApp/src/Fragments/Default.jsx
+++ FacultyApp/src/Fragments/Default.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Default = () => {
+  const getLocalStorage = ()=>{
+    const keys = localStorage.key(3)
+    console.log(keys+ localStorage.getItem(keys));
+  }
+  getLocalStorage()
   return (
     <div>Default</div>
   )
