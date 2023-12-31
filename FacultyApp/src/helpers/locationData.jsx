143271a284225bd6ecd90c31ee5037663f2c0fca
Srinivasan <jpsrinivasan38@gmail.com>
2023-12-31 17:50:54 +0530

    making all the location function in single file

--- FacultyApp/src/helpers/locationData.jsx
+++ FacultyApp/src/helpers/locationData.jsx
@@ -1,4 +1,4 @@
-const getLocation = () => {
+export const getLocation = () => {
     return new Promise((resolve, reject) => {
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
@@ -15,5 +15,26 @@ const getLocation = () => {
       }
     });
   };
-  
-  export default getLocation;
+  const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371; // Radius of the earth in km
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // Distance in km
+    return distance;
+  };
+  export const calculateAttendeeProximity = (attendee, userLocation) => {
+    if(!userLocation){return "0%"}
+    const distance = calculateDistance(
+      userLocation.latitude,
+      userLocation.longitude,
+      parseFloat(attendee.latitude),
+      parseFloat(attendee.longitude)
+    );
+    const maxProximity = 1; // Maximum proximity distance (in km)
+    const proximityPercentage = ((maxProximity - distance) / maxProximity) * 100;
+    return proximityPercentage.toFixed(2)>0? proximityPercentage.toFixed(2)+ '%':"0%";
+  };
