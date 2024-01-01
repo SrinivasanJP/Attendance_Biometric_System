9d6687d087862ee8b9af73b15501ea787e9fdf14
Srinivasan <jpsrinivasan38@gmail.com>
2024-01-01 10:25:56 +0530

    attendance table

---
+++ FacultyApp/src/Components/AttendeesTable.jsx
@@ -0,0 +1,30 @@
+import {calculateAttendeeProximity} from '../helpers/locationData';
+const AttendeesTable = ({attendees, userLocation}) => {
+    if(attendees.length === 0){
+      return <p>Loading...</p>
+    }
+    return (
+      <table className="min-w-full">
+        <thead className=' bg-gradient-to-r from-blue-300 to-slate-100 rounded-xl '>
+          <tr>
+            <th className='px-6 py-3 uppercase text-start text-sm font-medium w-[30%]'>Name</th>
+            <th className='px-6 py-3 uppercase text-start text-sm font-medium w-[20%]'>Register NO</th>
+            <th className='px-6 py-3 uppercase text-start text-sm font-medium'>Image</th>
+            <th className='px-6 py-3 uppercase text-start text-sm font-medium'>Proximity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {attendees.length<=0?(<tr>
+            <td colSpan={3} className='text-center px-6 py-3'>No Attendees</td></tr>):attendees.map((attendee, index) => (
+            <tr key={index} className='odd:bg-gray-100 '>
+              <td className={"px-6 py-4 whitespace-nowrap text-lg font-medium text-gray-800 border-2 border-gray-200"}>{attendee.userName}</td>
+              <td className={"px-6 py-4 whitespace-nowrap text-lg font-medium text-gray-800  border-2 text-center border-gray-200"}>{attendee.registerNo}</td>
+              <td className={"px-6 py-4 text-center w-[30%]"}><img className='inline-block' src={attendee.imageURL} alt="attendee image" width={100} /></td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800  border-2 text-center border-gray-200 w-[20%]">{calculateAttendeeProximity(attendee, userLocation)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
+  export default AttendeesTable
