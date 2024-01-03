ce7506f49c188426e6918e5db44d883e7af735fc
Srinivasan <jpsrinivasan38@gmail.com>
2024-01-03 17:48:22 +0530

    PermissionInstruction activity created

---
+++ app/src/main/java/dev/roxs/attendance/HelperActivities/PermissionInstruction.java
@@ -0,0 +1,16 @@
+package dev.roxs.attendance.HelperActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.roxs.attendance.R;
+
+public class PermissionInstruction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_instruction);
+    }
+}
