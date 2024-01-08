ced761ccaafa88d192e15423c210b72d489b423a
Srinivasan <jpsrinivasan38@gmail.com>
2024-01-08 10:29:32 +0530

    pin key event listener

--- app/src/main/java/dev/roxs/attendance/Activities/Setup.java
+++ app/src/main/java/dev/roxs/attendance/Activities/Setup.java
@@ -8,8 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -19,6 +21,7 @@ import android.widget.Toast;
 
 import com.chaos.view.PinView;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -35,14 +38,34 @@ public class Setup extends AppCompatActivity {
     private EditText vReg_no, vName;
     private ProgressBar vSetupProgress;
     private TextView vFinishSetup;
+    private Button button;
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private DocumentReference reference;
     private FingerPrint fp;
     private SharedpreferenceHelper sp;
     private RelativeLayout vContainer;
-    private PinView vPin;
+    private PinView vPin,pinView;
 
+    private void handlePin(DocumentSnapshot documentSnapshot){
+        String sPinView = Objects.requireNonNull(pinView.getText()).toString();
+        if(!sPinView.isEmpty()){
+            if(Objects.requireNonNull(documentSnapshot.get("pin")).toString().equals(sPinView)){
+                sp.addData(documentSnapshot.getId(), Objects.requireNonNull(documentSnapshot.get("registerNo")).toString(), Objects.requireNonNull(documentSnapshot.get("name")).toString());
+                startActivity(new Intent(getApplicationContext(), IDPage.class));
+                finish();
+            }else{
+                Toast.makeText(Setup.this, "Invalid PIN", Toast.LENGTH_SHORT).show();
+                pinView.setError("Invalid PIN");
+                pinView.setText("");
+                pinView.setLineColor(getColor(R.color.red));
+            }
+        }else{
+            Toast.makeText(Setup.this, "PIN is required", Toast.LENGTH_SHORT).show();
+            pinView.setError("PIN is required");
+            pinView.setLineColor(getColor(R.color.red));
+        }
+    }
     @Override
     protected void onStart() {
         super.onStart();
@@ -61,31 +84,18 @@ public class Setup extends AppCompatActivity {
                             LayoutInflater inflater = this.getLayoutInflater();
                             View view = inflater.inflate(R.layout.pin_get_layout,null);
                             alertDialog.setView(view);
-                            PinView pinView = view.findViewById(R.id.pinInput);
-                            Button button = view.findViewById(R.id.enter);
-                            button.setOnClickListener(v -> {
-                                String sPinView = Objects.requireNonNull(pinView.getText()).toString();
-                                if(!sPinView.isEmpty()){
-                                    if(Objects.requireNonNull(documentSnapshot.get("pin")).toString().equals(sPinView)){
-                                        Toast.makeText(Setup.this, Objects.requireNonNull(documentSnapshot.get("pin")).toString(), Toast.LENGTH_SHORT).show();
-                                    }else{
-                                        Toast.makeText(Setup.this, "Invalid PIN", Toast.LENGTH_SHORT).show();
-                                        pinView.setError("Invalid PIN");
-                                        pinView.setLineColor(getColor(R.color.red));
-                                    }
-                                }else{
-                                    Toast.makeText(Setup.this, "PIN is required", Toast.LENGTH_SHORT).show();
-                                    pinView.setError("PIN is required");
-                                    pinView.setLineColor(getColor(R.color.red));
-                                }
-
-                            });
+                            pinView = view.findViewById(R.id.pinInput);
+                            button = view.findViewById(R.id.enter);
+                            button.setOnClickListener(v -> handlePin(documentSnapshot));
                             alertDialog.create().show();
-//                            sp.addData(documentSnapshot.getId(), Objects.requireNonNull(documentSnapshot.get("registerNo")).toString(), Objects.requireNonNull(documentSnapshot.get("name")).toString());
-//                            startActivity(new Intent(getApplicationContext(), IDPage.class));
-//                            finish();
+                            pinView.setOnEditorActionListener((v, actionId, event) -> {
+                                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+                                    handlePin(documentSnapshot);
+                                }
+                                return false;
+                            });
+
                         } else {
-                            Toast.makeText(Setup.this, "Fingerprint not available", Toast.LENGTH_SHORT).show();
                             vContainer.setVisibility(View.VISIBLE);
                         }
                         }else{
