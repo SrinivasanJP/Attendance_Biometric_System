ee9f7e5d6e43f11202c777f1b548e2a616135646
Srinivasan <jpsrinivasan38@gmail.com>
2024-09-12 11:31:35 +0530

    fix: entry point has been changed

--- app/src/main/java/dev/roxs/attendance/Activities/Setup.java
+++ app/src/main/java/dev/roxs/attendance/Activities/Setup.java
@@ -70,57 +70,58 @@ public class Setup extends AppCompatActivity {
         }
     }
     @Override
-    protected void onStart() {
-        super.onStart();
-        sp = new SharedpreferenceHelper(this);
-        vPageProgress = findViewById(R.id.page_progress);
-        vPageProgress.setVisibility(View.VISIBLE);
-        vContainer = findViewById(R.id.setup_container);
-        vContainer.setVisibility(View.INVISIBLE);
-        if(sp.isDataAvailable()){
-            startActivity(new Intent(getApplicationContext(), IDPage.class));
-            finish();
-        }else{
-                fp = new FingerPrint(Setup.this);
-                fp.isFingerprintAvailable(db, fp.getFingerPrint(), (networkError,isAvailable, documentSnapshot) -> {
-                    if(!networkError){
-                        if (isAvailable) {
-                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-                            LayoutInflater inflater = this.getLayoutInflater();
-                            View view = inflater.inflate(R.layout.pin_get_layout,null);
-                            alertDialog.setView(view);
-                            pinView = view.findViewById(R.id.pinInput);
-                            button = view.findViewById(R.id.enter);
-                            button.setOnClickListener(v -> handlePin(documentSnapshot));
-                            alertDialog.setCancelable(false);
-                            AlertDialog dialog = alertDialog.create();
-                            Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                            dialog.show();
-                            pinView.setOnEditorActionListener((v, actionId, event) -> {
-                                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
-                                    handlePin(documentSnapshot);
-                                }
-                                return false;
-                            });
+//    protected void onStart() {
+//        super.onStart();
+//        sp = new SharedpreferenceHelper(this);
+//        vPageProgress = findViewById(R.id.page_progress);
+//        vPageProgress.setVisibility(View.VISIBLE);
+//        vContainer = findViewById(R.id.setup_container);
+//        vContainer.setVisibility(View.INVISIBLE);
+//        if(sp.isDataAvailable()){
+//            startActivity(new Intent(getApplicationContext(), FaceCapture.class));
+//            finish();
+//        }else{
+//                fp = new FingerPrint(Setup.this);
+//                fp.isFingerprintAvailable(db, fp.getFingerPrint(), (networkError,isAvailable, documentSnapshot) -> {
+//                    if(!networkError){
+//                        if (isAvailable) {
+//                            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+//                            LayoutInflater inflater = this.getLayoutInflater();
+//                            View view = inflater.inflate(R.layout.pin_get_layout,null);
+//                            alertDialog.setView(view);
+//                            pinView = view.findViewById(R.id.pinInput);
+//                            button = view.findViewById(R.id.enter);
+//                            button.setOnClickListener(v -> handlePin(documentSnapshot));
+//                            alertDialog.setCancelable(false);
+//                            AlertDialog dialog = alertDialog.create();
+//                            Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//                            dialog.show();
+//                            pinView.setOnEditorActionListener((v, actionId, event) -> {
+//                                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
+//                                    handlePin(documentSnapshot);
+//                                }
+//                                return false;
+//                            });
+//
+//                        } else {
+//                            vPageProgress.setVisibility(View.INVISIBLE);
+//                            vContainer.setVisibility(View.VISIBLE);
+//                        }
+//                        }else{
+//                            Toast.makeText(this, "No network", Toast.LENGTH_SHORT).show();
+//                        }
+//                });
+//
+//        }
+//
+//    }
 
-                        } else {
-                            vPageProgress.setVisibility(View.INVISIBLE);
-                            vContainer.setVisibility(View.VISIBLE);
-                        }
-                        }else{
-                            Toast.makeText(this, "No network", Toast.LENGTH_SHORT).show();
-                        }
-                });
-
-        }
-
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
+        sp = new SharedpreferenceHelper(this);
+        fp =new FingerPrint(Setup.this);
         //hooks
         vReg_no = findViewById(R.id.reg_no);
         vName = findViewById(R.id.name);
@@ -144,23 +145,27 @@ public class Setup extends AppCompatActivity {
             if(!sReg_no.isEmpty()){
                 if(!sName.isEmpty()){
                     if(!sPin.isEmpty()){
-                        Map<String, String> user = new HashMap<>();
-                        user.put("name", sName);
-                        user.put("registerNo", sReg_no);
-                        user.put("pin",sPin);
-                        reference.set(user).addOnCompleteListener(task -> {
-                            if(task.isSuccessful()){
-                                sp.addData(fp.getFingerPrint(),sReg_no, sName);
+//                        Map<String, String> user = new HashMap<>();
+//                        user.put("name", sName);
+//                        user.put("registerNo", sReg_no);
+//                        user.put("pin",sPin);
+//                        reference.set(user).addOnCompleteListener(task -> {
+//                            if(task.isSuccessful()){
+//                                sp.addData(fp.getFingerPrint(),sReg_no, sName);
+
                                 Intent intent = new Intent(getApplicationContext(), FaceCapture.class);
+                                intent.putExtra("name",sName);
+                                intent.putExtra("registerNo", sReg_no);
+                                intent.putExtra("pin",sPin);
                                 startActivity(intent);
                                 finish();
-                            }else{
-                                Log.d("UT error", "onCreate: "+task.getException());
-                                Toast.makeText(Setup.this, "Check your internet connection", Toast.LENGTH_SHORT).show();
-                                vSetupProgress.setVisibility(View.INVISIBLE);
-                                vFinishSetup.setVisibility(View.VISIBLE);
-                            }
-                        });
+//                            }else{
+//                                Log.d("UT error", "onCreate: "+task.getException());
+//                                Toast.makeText(Setup.this, "Check your internet connection", Toast.LENGTH_SHORT).show();
+//                                vSetupProgress.setVisibility(View.INVISIBLE);
+//                                vFinishSetup.setVisibility(View.VISIBLE);
+//                            }
+//                        });
                     }else{
                         vPin.setError("pin is required");
                     }
