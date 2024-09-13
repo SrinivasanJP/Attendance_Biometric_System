f14fd73937dfc7fe4b1d57fb28d90bd133b7d0f9
Srinivasan <jpsrinivasan38@gmail.com>
2024-09-13 11:06:23 +0530

    splitted data send

--- app/src/main/java/dev/roxs/attendance/Helper/CLassifierInterface.java
+++ app/src/main/java/dev/roxs/attendance/Helper/CLassifierInterface.java
@@ -1,22 +1,18 @@
 package dev.roxs.attendance.Helper;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public interface CLassifierInterface {
     class Recognition {
-        private final String name;
-        private final String regNo;
-        private final String pin;
         private final Float distance;
-        private ArrayList<float[][]> extra;
-        public Recognition(final String name, final String regNo, final  String pin, final Float distance) {
-            this.name = name;
-            this.regNo = regNo;
-            this.pin = pin;
+        private List<Float> extra;
+        private int row=0,col=0;
+        public Recognition(final Float distance) {
             this.distance = distance;
             this.extra = null;
         }
-        public void setExtra(ArrayList<float[][]> extra) {
+        public void setExtra(List<Float> extra) {
             this.extra = extra;
         }
         public Object getExtra() {
@@ -25,21 +21,32 @@ public interface CLassifierInterface {
         @Override
         public String toString() {
             String resultString = "";
-            if (name != null) {
-                resultString += "[" + name + "] ";
-            }
-
-            if (regNo != null) {
-                resultString += regNo + " ";
-            }
-            if(pin != null){
-                resultString += pin +" ";
-            }
             if (distance != null) {
                 resultString += String.format("(%.1f%%) ", distance * 100.0f);
             }
 
             return resultString.trim();
+        }
+        public List<Float> flatten2DArray(float[][] array) {
+            List<Float> flatList = new ArrayList<>();
+            for (float[] row : array) {
+                this.row++;
+                for (float value : row) {
+                    flatList.add(value);
+                    this.col++;
+                }
+            }
+            return flatList;
+        }
+        public float[][] unflattenList(List<Float> flatList) {
+            float[][] array = new float[this.row][this.col];
+            int index = 0;
+            for (int i = 0; i < this.row; i++) {
+                for (int j = 0; j < this.col; j++) {
+                    array[i][j] = flatList.get(index++);
+                }
+            }
+            return array;
         }
     }
 }
