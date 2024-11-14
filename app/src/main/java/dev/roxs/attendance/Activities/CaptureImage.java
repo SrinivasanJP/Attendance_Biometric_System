8a18dbf64675a1f2cd28d71e3f9a7a341ccf4461
Srinivasan <jpsrinivasan38@gmail.com>
2024-11-14 16:52:32 +0530

    feat: captureImage ui funtion

--- app/src/main/java/dev/roxs/attendance/Activities/CaptureImage.java
+++ app/src/main/java/dev/roxs/attendance/Activities/CaptureImage.java
@@ -4,8 +4,12 @@ package dev.roxs.attendance.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.view.PreviewView;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import dev.roxs.attendance.Helper.FaceRecognitionHelper;
 import dev.roxs.attendance.Helper.SharedpreferenceHelper;
@@ -14,6 +18,13 @@ import dev.roxs.attendance.R;
 public class CaptureImage extends AppCompatActivity implements FaceRecognitionHelper.FaceRecognitionCallback {
 
     private Intent preIntent;
+    ImageView face_preview;
+    RelativeLayout confirmBtn, retakeBtn;
+    Boolean startRocognize;
+
+    Bitmap sendImage,storeImage;
+
+    FaceRecognitionHelper faceRecognitionHelper;
 
 
     @Override
@@ -21,21 +32,58 @@ public class CaptureImage extends AppCompatActivity implements FaceRecognitionHe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture_image);
         preIntent = getIntent();
+        face_preview = findViewById(R.id.previewImage);
+        confirmBtn = findViewById(R.id.finishSetup);
+        retakeBtn = findViewById(R.id.retake);
+        confirmBtn.setVisibility(View.INVISIBLE);
+        retakeBtn.setVisibility(View.INVISIBLE);
+
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faceRecognitionHelper.saveToInternalStorage(storeImage, sendImage);
+                Intent sendData = new Intent(getApplicationContext(), SendAttendanceData.class);
+                sendData.putExtra("sessionID",preIntent.getStringExtra("sessionID"));
+                sendData.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(sendData);
+                finish();
+
+            }
+        });
+
+        retakeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                faceRecognitionHelper.setRecognition(true);
+                face_preview.setVisibility(View.INVISIBLE);
+                confirmBtn.setVisibility(View.INVISIBLE);
+                retakeBtn.setVisibility(View.INVISIBLE);
+            }
+        });
+
         PreviewView previewView = findViewById(R.id.previewView);
         TextView textProgress = findViewById(R.id.textProgress);
         TextView textNote = findViewById(R.id.textNote);
         SharedpreferenceHelper sp = new SharedpreferenceHelper(getApplicationContext());
 
-        FaceRecognitionHelper faceRecognitionHelper = new FaceRecognitionHelper(CaptureImage.this, sp.getStoredEmbeddings(), this);
+        faceRecognitionHelper = new FaceRecognitionHelper(CaptureImage.this, sp.getStoredEmbeddings(), this);
         faceRecognitionHelper.cameraBind(getApplicationContext(), previewView, textNote, textProgress);
     }
 
     @Override
-    public void onDistanceCalculated(float distance) {
-        Intent sendData = new Intent(getApplicationContext(), SendAttendanceData.class);
-        sendData.putExtra("sessionID",preIntent.getStringExtra("sessionID"));
-        sendData.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(sendData);
-        finish();
+    public void onDistanceCalculated(float distance, Bitmap storeImage, Bitmap sendImage) {
+        this.sendImage = sendImage;
+        this.storeImage = storeImage;
+        faceRecognitionHelper.setRecognition(false);
+
+        // Update UI on the main thread
+        runOnUiThread(() -> {
+            face_preview.setImageBitmap(sendImage);
+
+            face_preview.setVisibility(View.VISIBLE);
+            retakeBtn.setVisibility(View.VISIBLE);
+            confirmBtn.setVisibility(View.VISIBLE);
+        });
     }
+
 }
